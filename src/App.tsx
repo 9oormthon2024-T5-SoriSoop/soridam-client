14b3d10b9cf87fe8b2a7efd363e89aca91a110b3
Elvin-lee <herjun802@gmail.com>
2024-12-15 11:32:18 +0900

    삭제 모달

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom'
 import './App.css'
 import { Fragment } from 'react'
@@ -8,26 +7,47 @@ import NavBar from './layout/navBar/NavBar'
 import NoiseRegister from './page/register/NoiseRegister'
 import NoiseList from './page/noiseList/NoiseList'
 import MeasureInfoPopup from './component/standard/NoiseStandard'
-import { useAppSelector } from './hook/redux'
+import { useAppSelector, useAppDispatch } from './hook/redux'
 import NoiseMap from './page/noiseMap/NoiseMap'
 import DeleteModal from './component/deleteModal/DeleteModal'
+import { toggleDeleteModal } from './store/menu/menuSlice' // 액션 가져오기
 
 function AppContent() {
   const location = useLocation();
+  const dispatch = useAppDispatch(); // dispatch를 사용하기 위한 훅
   const { modalOpen, delModalOpen } = useAppSelector((state) => state.menu);
+
+  // 삭제 확인 시 실행할 함수
+  const handleDelete = () => {
+    console.log('삭제되었습니다.');
+    dispatch(toggleDeleteModal(false)); // 모달 닫기
+  };
+
+  // 모달 닫기 함수
+  const handleClose = () => {
+    dispatch(toggleDeleteModal(false)); // 모달 닫기
+  };
 
   return (
     <Fragment>
       <Routes>
         <Route path='/login' element={<Login />} />
         <Route path='/measure' element={<Noise />} />
-        <Route path='/register' element={<NoiseRegister/>} />
-        <Route path='/save' element={<NoiseList/>} />
-        <Route path='/' element={<NoiseMap/>} />
+        <Route path='/register' element={<NoiseRegister />} />
+        <Route path='/save' element={<NoiseList />} />
+        <Route path='/' element={<NoiseMap />} />
       </Routes>
       {location.pathname !== '/login' && <NavBar />}
-      { modalOpen ? <MeasureInfoPopup/> : ''}
-      { delModalOpen ? <DeleteModal /> : ''}
+      {modalOpen ? <MeasureInfoPopup /> : ''}
+      {delModalOpen ? (
+        <DeleteModal
+          isOpen={delModalOpen}
+          onClose={handleClose}
+          onDelete={handleDelete}
+        />
+      ) : (
+        ''
+      )}
     </Fragment>
   );
 }
@@ -40,4 +60,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
--- src/component/deleteModal/DeleteModal.styles.ts
+++ src/component/deleteModal/DeleteModal.styles.ts
@@ -1,8 +1,20 @@
 import styled from "styled-components";
 
-export const ModalBackground = styled.div`
+export const Background = styled.div`
+  width: 375px;
+  height: 812px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background: rgba(0, 0, 0, 0.6);
+`
+
+export const ModalContainer = styled.div`
     width: 299px;
     height: 152px;
     background-color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 `
 
